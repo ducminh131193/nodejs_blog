065a269e75286e211b89dc2a1833b71da1b8f71c
minh <ducminh131193@gmail.com>
2022-04-19 10:30:53 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+app.use(express.json());
+
+app.get("/testjs", (req, res) => {
+    var a = 2;
+    var b = 5;
+
+    var c = a + b;
+  res.send("Hello World!");
+});
+
+// app.post("/minhbeo", (req, res) => {
+//     if (req.body.username) {
+//         res.status(200).json({
+//             success: true,
+//             username: req.body.username
+//         });
+//     } else {
+//         res.status(404).json({
+//             success: false
+//         });
+//     }
+   
+//   });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
